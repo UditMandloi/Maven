b9aaf0f49105e1b1f62d3bd6466b19045eeeb227
Udit <Udit@192.168.1.3>
2017-11-10 09:38:36 +0530

    WebService Post

--- UdPro/Web/src/main/java/com/udit/controller/ws/RegistrationControllerWebService.java
+++ UdPro/Web/src/main/java/com/udit/controller/ws/RegistrationControllerWebService.java
@@ -1,15 +1,23 @@
 package com.udit.controller.ws;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.udit.model.RegistrationDTO;
+import com.udit.repository.RegistrationRepository;
 
 @Controller
 public class RegistrationControllerWebService {
+	
+	@Autowired
+	private RegistrationRepository registrationRepository;
+	
+	
 	
 	@RequestMapping(method=RequestMethod.GET,value="/registration/{id}")
 	public @ResponseBody RegistrationDTO getRegistration(@PathVariable Integer id)
@@ -23,4 +31,11 @@ public class RegistrationControllerWebService {
 		return registrationDTO;
 	}
 	
+	@RequestMapping(value = "/registration/save", method = RequestMethod.POST)
+	@ResponseBody
+	public RegistrationDTO save(@RequestBody RegistrationDTO registrationDTO) {
+		registrationRepository.registrationSave(registrationDTO);
+		return registrationDTO;
+
+	}
 }
